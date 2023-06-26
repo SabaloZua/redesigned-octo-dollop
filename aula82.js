915391e4717e1d89a45b859028b44b3af557f151
AstronautaSZ <133335791+AstronautaSZ@users.noreply.github.com>
2023-06-26 22:20:07 +0100

    Add files via upload
    Neste código eu fiz um palco com um monte de bolinhas flutuando utilizando  orientação a objectos em javascrip.

---
+++ aula82.js
@@ -0,0 +1,111 @@
+ const btn_remover=document.querySelector('#btn_remover');
+ const btn_add=document.querySelector("#btn_add")
+ const num_objectos=document.querySelector("#num_objectos")
+ const palco=document.getElementById("palco")
+ const txt_qtde=document.querySelector("#txt_qtde")
+ const controle=document.querySelector("#controle")
+
+ let largura_palco=palco.offsetWidth;
+ let altura_palco=palco.offsetHeight;
+ let bolas=[]
+ let num_bola=0
+
+class Bola{
+   constructor(arrybolas,palco){
+      this.tam=Math.floor(Math.random()*15)+10
+      this.r=Math.floor(Math.random()*255)
+      this.g=Math.floor(Math.random()*255)
+      this.b=Math.floor(Math.random()*255)
+      this.px=Math.floor(Math.random()*(largura_palco-this.tam))
+      this.py=Math.floor(Math.random()*(altura_palco-this.tam))
+       this.velx=Math.floor(Math.random()*2)+0.5
+      this.vely=Math.floor(Math.random()*2)+0.5
+      this.dirx=Math.floor((Math.random()*10)) > 5 ? 1 :-1
+      this.diry=Math.floor((Math.random()*10)) > 5 ? 1 :-1
+      this.palco=palco;
+      this.arrybolas=arrybolas
+      this.id=Date.now()+"_"+Math.floor(Math.random()*10000000000000)
+     this.desenhar()
+      this.controle=setInterval(this.controlar1,10);
+      this.eu=document.getElementById(this.id)
+      num_bola++
+      num_objectos.innerHTML=num_bola
+   }
+   mpos=()=>{
+        
+      return this.arrybolas.indexOf(this)
+   }
+
+   
+   remover=()=>{
+         clearInterval(this.controle)
+         bolas=bolas.filter((b)=>{
+            if(b.id!=this.id)
+            return b
+         })
+         this.eu.remove()
+         num_bola-- 
+         num_objectos.innerHTML=num_bola
+   }
+
+   desenhar=()=>{
+      const div=document.createElement('div')
+      div.setAttribute("id",this.id)
+      div.setAttribute('class',"bola")
+      div.setAttribute('style',`left:${this.px}px; top:${this.py}px;
+      width:${this.tam}px; height:${this.tam}px; background-color:rgb(${this.r},${this.g},${this.b});`)
+      this.palco.appendChild(div)
+   }
+      colidir=()=>{
+         if(this.px+this.tam>=largura_palco){
+            this.dirx=-1
+         }else if(this.px<=0){
+            this.dirx=1
+         }
+         if(this.py+this.tam>=altura_palco){
+            this.diry=-1
+         }else if(this.py<=0){
+            this.diry=1
+         }
+      }   
+   controlar1=()=>{
+         this.colidir()
+      this.px+=this.dirx*this.velx
+      this.py+=this.diry*this.vely
+      this.eu.setAttribute('style',`left:${this.px}px; top:${this.py}px;
+      width:${this.tam}px; height:${this.tam}px; background-color:rgb(${this.r},${this.g},${this.b});`)
+       if((this.py > altura_palco)|| (this.px > largura_palco)){
+       this.remover()
+       }
+   } 
+
+}
+
+ window.addEventListener("resize",()=>{
+    
+ let largura_palco=palco.offsetWidth;
+ let autora_palco=palco.offsetHeight;        
+   
+ })
+
+ btn_add.addEventListener('click',()=>{
+         const qtde=txt_qtde.value;  
+         for(let i=0;i<qtde;i++){
+            //intaciar novas bolinhas
+            bolas.push(new Bola(bolas,palco)) 
+         }
+      
+ })
+
+     
+ btn_remover.addEventListener("click",()=>{
+   
+   bolas.map((b)=>{
+         // remover bolinha
+
+      b.remover();
+
+
+   })
+
+ })
